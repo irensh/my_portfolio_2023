f519c438b1d2f82b606d8a8782a6d2e19fd4ebcf
Irena Popova <kopoirga@gmail.com>
2023-04-05 15:50:18 +0300

    portfolio v2.0
    replaced script.js (spageti code) with load-data.js (add classes)

---
+++ load-data.js
@@ -0,0 +1,18 @@
+const selectors = {
+    rightPanel: '#right-panel',
+    closeBtn: '#close',
+    divContentInfo: '#content-info',
+    allShortInfoFa: '.short-info > p > i.fa'
+}
+
+class LoadLabels {
+    #isOpen;
+
+    constructor() {
+        this.isOpen = false;
+    }
+
+    init() {
+
+    }
+}
